173ed3f4dcd769d5eb633d8be7320c8bb8877c5f
jun6000 <arjunvishanth@gmail.com>
2022-07-04 00:47:55 +0530

    Refactored checkGuess function.

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+const guess = document.getElementById("guess"); // Guess field
+const submit = document.getElementById("submitButton"); // Submit button
+const form = document.getElementById("guessForm"); // Submit form consisting of guess field and submit button elements
+const prevGuesses = document.getElementById("prevGuesses"); // Section that shows guesses so far
+const resultBanner = document.getElementById("resultBanner"); // Result banner
+const result = document.getElementById("result"); // Result message
+let i = 10; // Max guesses (turns) allowed
 const randomNumber = getRandom(1, 101);
-// alert(randomNumber);
-const guess = document.getElementById("guess");
-const submit = document.getElementById("submitButton");
-const form = document.getElementById("guessForm");
-let i = 10; // counter for max number of guesses allowed
 
-// Enter does nothing
+// ENTER does nothing
 form.addEventListener("submit", e => {
     e.preventDefault();
 });
@@ -15,27 +17,31 @@ function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-// Function to show guesses
-function checkGuess(value) {
-    // Show result section if not displayed
-    const prevGuesses = document.getElementById("prevGuesses");
-    const resultBanner = document.getElementById("resultBanner");
-    const result = document.getElementById("result");
+// Game win function
+function gameWin() {
+    resultBanner.style.backgroundColor = "green";
+    resultBanner.textContent = "Congratulations! You got it right!";
+    result.style.display = "none";        
+
+    document.getElementById("replayButton").style.display = "block"; // Show replay button
+}
+
+// Show result section
+function showResult(value) {
+
+    // Unhide hidden elements to show result
     if(prevGuesses.style.display !== "block") prevGuesses.style.display = "block";
     if(resultBanner.style.display !== "block") resultBanner.style.display = "block";
 
-    // Append value to previous guesses
-    prevGuesses.textContent += " " + value;
+    prevGuesses.textContent += " " + value; // Append value to previous guesses
+}
 
-    // Check if the guess matches
-    if(value == randomNumber) {
-        // Change banner & result
-        resultBanner.style.backgroundColor = "green";
-        resultBanner.textContent = "Congratulations! You got it right!";
-        result.style.display = "none";        
-
-        // Show replay button
-        document.getElementById("replayButton").style.display = "block";
+// Function to check guesses
+function checkGuess(value) {
+    showResult(value);
+    
+    if(value == randomNumber) {// Check if the guess matches
+        gameWin(); // Change banner & result
 
         return true;
     }
@@ -56,28 +62,31 @@ function gameOver() {
     document.getElementById("replayButton").style.display = "block";
 }
 
+// Disables submit button and guess field
+function disableGuessing() {
+    submit.disabled = true;
+    guess.disabled = true;
+    submit.removeEventListener("click", getGuess);
+}
+
 // Get Guess function
 function getGuess() {
     const value = guess.value;
-    guess.value = "";
+    guess.value = ""; // Resetting the guess field to blank
     if(!value) alert("Please enter a valid guess!");
     else {
-        i--; // Decrement counter
+        i--; // Decrement number of turns left
         let r = checkGuess(value);
         if(r === true || i === 0) {
-            submit.disabled = true;
-            guess.disabled = true;
-            submit.removeEventListener("click", getGuess);
-
-            // Game over guess is incorrect
-            if(r === false) gameOver();
+            disableGuessing();
+            if(r === false) gameOver(); // Game over if guess is incorrect and turns are over
         }
     }
-    guess.focus();
+    guess.focus(); // Refocus the guess field
 }
 
 // Main
-submit.addEventListener("click", getGuess);
-document.getElementById("replayButton").addEventListener("click", () => {
+submit.addEventListener("click", getGuess); // A guess is submitted on clicking the submit button
+document.getElementById("replayButton").addEventListener("click", () => { // Reload page for new game
     window.location.reload();
 });
